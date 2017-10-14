3a48c97b17f710e72cb2c1a185964af63636f16b
Lara <Lara Devitt>
2017-10-14 07:36:56 -0600

    Apply the effect to the element only once.

--- js/operating-hours-block.js
+++ js/operating-hours-block.js
@@ -2,14 +2,18 @@
   Drupal.behaviors.OperatingHoursBlock = {
     attach: function (context, settings) {
 
-      // We are using JS to select the day so it doesn't get caught in cache.
-      var days = drupalSettings.operating_hours_block.opHoursBlock.days;
-      var today = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][(new Date()).getDay()];
-      var hours = days[today.toLowerCase()]['#markup'];
+      // Apply the effect to element only once.
+      $('#operating-hours-today', context).once('operatingHoursToday').each(function() {
+        // We are using JS to select the day so it doesn't get caught in cache.
+        var days = drupalSettings.operating_hours_block.opHoursBlock.days;
+        var today = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][(new Date()).getDay()];
+        var hours = days[today.toLowerCase()]['#markup'];
 
-      if (typeof hours !== 'undefined') {
-        $('#operating-hours-today').text('Today: ' + hours);
-      }
+        if (typeof hours !== 'undefined') {
+          $('#operating-hours-today').text('Today: ' + hours);
+        }
+      });
+
     }
   }
 })(jQuery, Drupal, drupalSettings);
